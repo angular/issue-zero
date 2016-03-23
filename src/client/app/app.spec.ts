81019177b96a60dc115cb48fc104236cc7ccaa7d
Jeff Cross <middlefloor@gmail.com>
2016-03-23 15:58:38 -0700

    test: add testing setup with basic unit test

---
+++ src/client/app/app.spec.ts
@@ -0,0 +1,16 @@
+import {
+  it,
+  injectAsync,
+  TestComponentBuilder
+} from 'angular2/testing';
+
+import {AppComponent} from './app';
+
+describe('AppComponent', () => {
+  it('should say hello', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    return tcb.createAsync(AppComponent)
+      .then(fixture => {
+        expect(fixture.nativeElement.textContent).toBe('Hello!');
+      });
+  }));
+});
--- src/client/typings.d.ts
+++ src/client/typings.d.ts
@@ -1,1 +1,2 @@
 /// <reference path="../../typings/browser/ambient/es6-shim/index.d.ts" />
+/// <reference path="../../typings/browser/ambient/jasmine/index.d.ts" />
