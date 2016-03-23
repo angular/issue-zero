2c5008ee2a35d7d1c532762f23b1f518c0b19a83
Jeff Cross <middlefloor@gmail.com>
2016-03-23 15:17:45 -0700

    feat(client): add basic client app files

---
+++ src/client/app/app.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'issue-zero-app',
+  templateUrl: 'app/app.html'
+})
+export class AppComponent {
+}
---
+++ src/client/typings.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="../../typings/browser/ambient/es6-shim/index.d.ts" />
