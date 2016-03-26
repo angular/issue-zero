d9b140afc3464a0716228c991c5b8e4058e397f8
Jeff Cross <middlefloor@gmail.com>
2016-03-25 20:17:55 -0700

    feat(app): add toolbar to app component

--- src/client/app/app.ts
+++ src/client/app/app.ts
@@ -1,15 +1,29 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {MdToolbar} from '@angular2-material/toolbar';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import {MdButton} from '@angular2-material/button';
 
 import {IssuesComponent} from './issues/issues';
 
 @Component({
   selector : 'issue-zero-app',
+  styles : [ `
+    md-toolbar button {
+      color: white;
+      background: transparent;
+      outline: none;
+      border: none;
+    }
+  ` ],
   template : `
-    <h1>Hello!</h1>
+    <md-toolbar color="primary">
+      <span>Issue Zero</span>
+    </md-toolbar>
     <router-outlet></router-outlet>
   `,
-  directives : ROUTER_DIRECTIVES,
+  directives :
+      [ ROUTER_DIRECTIVES, MdToolbar, MD_SIDENAV_DIRECTIVES, MdButton ],
   providers : []
 })
 @RouteConfig(
