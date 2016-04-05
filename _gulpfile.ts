07ac04fad4a43bcde5b9c6ca82e6cd482385307f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-04-05 10:12:48 -0700

    refactor(build): Use System Builder to bundle the app.

--- _gulpfile.ts
+++ _gulpfile.ts
@@ -1,16 +1,22 @@
 /// <reference path="typings/main/ambient/node/index.d.ts" />
+
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var fse = require('fs-extra');
 var runSequence = require('run-sequence');
+var Builder = require('systemjs-builder');
 
 const clientTsProject = ts.createProject('src/client/tsconfig.json', {
   typescript: require('typescript'),
 });
 
-const clientRoot = 'src/client';
-const distClientRoot = 'dist/client';
-const clientTsTree = [`${clientRoot}/app/**/*.ts`, 'typings/browser/browser.d.ts'];
+const systemCfg = JSON.parse(fse.readFileSync('system.config.json'));
+
+const CLIENT_ROOT = 'src/client';
+const DIST_CLIENT_ROOT = 'dist/client';
+const TMP_CLIENT_ROOT = 'tmp/client';
+
+const clientTsTree = [`${CLIENT_ROOT}/app/**/*.ts`, 'typings/browser/browser.d.ts'];
 const clientVendorDeps = [
   'node_modules/angular2/**/*.js',
   'node_modules/angularfire2/**/*.js',
@@ -22,52 +28,73 @@ const clientVendorDeps = [
   'node_modules/@angular2-material/**/*.+(js|css|html|map)',
   'node_modules/material-design-icons/**/*.+(css|svg|woff|ttf|eot|woff2)'
 ];
-const clientHtmlTree = [`${clientRoot}/**/*.html`];
-const clientCssTree = [`${clientRoot}/**/*.css`];
+const clientHtmlTree = [`${CLIENT_ROOT}/**/*.html`];
+const clientCssTree = [`${CLIENT_ROOT}/**/*.css`];
 
 gulp.task('clean', (done) => {
   return fse.remove('dist', done);
 });
 
+gulp.task('bundle:client', ['build:client'], (done) => {
+  let builder = new Builder();
+  builder.config(systemCfg);
+  builder.bundle('tmp/client/app/index.js', 'dist/client/app.js')
+    .then(() => done());
+});
+
 gulp.task('build:client', ['clean'], (callback) => {
   runSequence(
-    'build:client/typescript',
-    'build:client/html',
-    'build:client/css',
-    'build:client/firebase',
-    'build:client/vendor',
+    [
+      'build:client/typescript',
+      'copy:client'
+    ],
     callback);
 });
 
+gulp.task('copy:client', [
+  'copy:client/html',
+  'copy:client/css',
+  'copy:client/firebase',
+  'copy:client/vendor/src',
+  'copy:client/vendor/mdfont'
+]);
 
-gulp.task('build:client/vendor', () => {
-  return gulp.src(clientVendorDeps, {base: 'node_modules'})
-      .pipe(gulp.dest(`${distClientRoot}/vendor`));
+gulp.task('copy:client/vendor/src', () => gulp
+  .src([
+    'node_modules/zone.js/dist/long-stack-trace-zone.js',
+    'node_modules/systemjs/dist/system.js',
+    'node_modules/angular2/bundles/angular2-polyfills.js'
+  ])
+  .pipe(gulp.dest(`${DIST_CLIENT_ROOT}/vendor`)))
+
+gulp.task('copy:client/vendor/mdfont', () => gulp
+  .src([
+    'node_modules/material-design-icons/iconfont/**/*'
+  ], {base: 'node_modules'})
+  .pipe(gulp.dest(`${DIST_CLIENT_ROOT}/vendor`)));
+
+gulp.task('copy:client/firebase', () => {
+  return gulp.src(`${CLIENT_ROOT}/firebase.json`)
+      .pipe(gulp.dest(DIST_CLIENT_ROOT));
 });
 
 
-gulp.task('build:client/firebase', () => {
-  return gulp.src(`${clientRoot}/firebase.json`)
-      .pipe(gulp.dest(distClientRoot));
-});
-
-
-gulp.task('build:client/html', () => {
+gulp.task('copy:client/html', () => {
   return gulp.src(clientHtmlTree)
-    .pipe(gulp.dest(distClientRoot));
+    .pipe(gulp.dest(DIST_CLIENT_ROOT));
 });
 
 
 gulp.task('build:client/typescript', () => {
   return gulp.src(clientTsTree.concat('typings/browser/ambient/**/*.ts'))
     .pipe(ts(clientTsProject))
-    .pipe(gulp.dest(`${distClientRoot}/app`))
+    .pipe(gulp.dest(`${TMP_CLIENT_ROOT}/app`))
 });
 
 
-gulp.task('build:client/css', () => {
+gulp.task('copy:client/css', () => {
   return gulp.src(clientCssTree)
-    .pipe(gulp.dest(distClientRoot));
+    .pipe(gulp.dest(DIST_CLIENT_ROOT));
 });
 
 
@@ -105,6 +132,6 @@ function doCheckFormat() {
   var clangFormat = require('clang-format');
   var gulpFormat = require('gulp-clang-format');
 
-  return gulp.src(`${clientRoot}/**/*.ts`)
+  return gulp.src(`${CLIENT_ROOT}/**/*.ts`)
       .pipe(gulpFormat.checkFormat('file', clangFormat));
 }
