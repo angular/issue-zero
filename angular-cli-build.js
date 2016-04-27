6206d2b26bceb6bda2ae61a013ec0988aabb2fbc
Jeff Cross <middlefloor@gmail.com>
2016-04-27 14:50:42 -0700

    chore(build): make build much faster by narrowing globs
    The material-icons glob patterns were too broad, causing
    broccoli to look over many files for matches, adding multiple
    seconds to the build. This takes it from ~7s to <1s

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -26,7 +26,7 @@ module.exports = function(defaults) {
       'firebase/lib/firebase-web.js',
       'rxjs/bundles/Rx.js',
       '@angular2-material/**/*.+(js|css|svg|map)',
-      'material-design-icons/**/*.+(woff2|woff)',
+      'material-design-icons/iconfont/MaterialIcons-Regular.+(woff|woff2)',
       'angular2-service-worker/dist/worker.js',
       'hammerjs/hammer.min.js'
     ]
